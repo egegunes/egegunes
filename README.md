41f692accfe4284d1d766c61cf473c9c70f50247
Ege Güneş <egegunes@gmail.com>
2020-07-10 19:47:32 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<table><tr><td valign="top">
+
+### Recent releases
+<!-- recent_releases starts -->
+* [gitlabci 0.3.1](https://github.com/egegunes/gitlabci/releases/tag/0.3.1) - 2020-05-12T07:43:03Z
+* [redmine-cli 1.3.0](https://github.com/egegunes/redmine-cli/releases/tag/1.3.0) - 2020-01-08T07:11:39Z
+* [gitlabenv 0.2.0](https://github.com/egegunes/gitlabenv/releases/tag/0.2.0) - 2019-08-20T19:17:10Z
+<!-- recent_releases ends -->
+</td><td valign="top">
+
+### Recent posts
+<!-- blog starts -->
+* [Renew SSL Certs on Azure Application Gateway with Gitlab CI](https://ege.dev/post/renew-ssl-certs-in-azure-app-gateway-using-gitlabci/)
+* [Building a Linux Kernel Module](https://ege.dev/post/building-a-kernel-module/)
+* [PortQuiz.net](https://ege.dev/post/portquiz-net/)
+* [Merging gitlabci and gitlabenv](https://ege.dev/post/merging-gitlabci-and-gitlabenv/)
+* [New project: gitlabci](https://ege.dev/post/new-project-gitlabci/)
+<!-- blog ends -->
+</td>
+
+</tr></table>
+
---
+++ build.py
@@ -0,0 +1,100 @@
+from datetime import datetime
+import re
+import os
+
+from python_graphql_client import GraphqlClient
+import feedparser
+
+
+TOKEN = os.getenv("GITHUB_TOKEN")
+
+
+def recent_releases():
+    query = """
+    query {
+        viewer {
+            repositories(first: 100, privacy: PUBLIC) {
+            pageInfo {
+                hasNextPage
+                endCursor
+            }
+            nodes {
+                name
+                releases(last:1) {
+                totalCount
+                nodes {
+                    name
+                    publishedAt
+                    url
+                }
+                }
+            }
+            }
+        }
+    }
+    """
+
+    client = GraphqlClient(endpoint="https://api.github.com/graphql")
+    data = client.execute(
+        query=query, headers={"Authorization": "Bearer {}".format(TOKEN)}
+    )
+
+    releases = []
+    repos = set()
+
+    for repo in data["data"]["viewer"]["repositories"]["nodes"]:
+        if repo["releases"]["totalCount"] and repo["name"] not in repos:
+            repos.add(repo["name"])
+            releases.append(
+                {
+                    "repo": repo["name"],
+                    "release": repo["releases"]["nodes"][0]["name"],
+                    "url": repo["releases"]["nodes"][0]["url"],
+                    "published": repo["releases"]["nodes"][0]["publishedAt"],
+                }
+            )
+
+    releases = sorted(
+        releases,
+        key=lambda r: datetime.strptime(r["published"], "%Y-%m-%dT%H:%M:%SZ"),
+        reverse=True,
+    )
+
+    return releases[:5]
+
+
+def recent_posts():
+    posts = feedparser.parse("https://ege.dev/index.xml")["entries"]
+
+    return posts[:5]
+
+
+def replace_chunk(content, marker, chunk):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
+
+    return r.sub(chunk, content)
+
+
+if __name__ == "__main__":
+    with open("README.md") as readme:
+        readme_contents = readme.read()
+
+    releases = recent_releases()
+    releases_md = "\n".join(
+        [
+            "* [{repo} {release}]({url}) - {published}".format(**release)
+            for release in releases
+        ]
+    )
+    rewritten = replace_chunk(readme_contents, "recent_releases", releases_md)
+
+    posts = recent_posts()
+    posts_md = "\n".join(["* [{title}]({link})".format(**post) for post in posts])
+    rewritten = replace_chunk(rewritten, "blog", posts_md)
+
+    with open("README.md", "w") as readme:
+        readme.write(rewritten)
